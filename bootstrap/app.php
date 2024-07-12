455bc6e41e163905667ed73120a23a487fd605c6
whlives <whlives@qq.com>
2024-07-12 15:07:04 +0800

    屏蔽空字符转null

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -34,6 +34,7 @@ return Application::configure(basePath: dirname(__DIR__))
             \Illuminate\Foundation\Http\Middleware\PreventRequestsDuringMaintenance::class,
             \Illuminate\Http\Middleware\ValidatePostSize::class,
             \Illuminate\Foundation\Http\Middleware\TrimStrings::class,
+            //\Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
